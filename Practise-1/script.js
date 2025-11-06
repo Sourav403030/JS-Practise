52691827c313ae7ce555aebe32833e4f50a9876b
Sourav Sanjay <souravsanjay07@gmail.com>
2025-11-06 09:59:01 +0530

    Question 3 completed

--- Practise-1/script.js
+++ Practise-1/script.js
@@ -119,3 +119,15 @@
 //     }
 // }
 
+//-------------------QUESTION 3------------------//
+// Count how many numbers between 1 and 15 are greater than 8
+// Loop and count conditionally.
+
+// let numbers = 0;
+// for(let i = 1; i <= 15; i++){
+//     if(i > 8){
+//         numbers++;
+//     }
+// }
+
+// console.log(numbers);
