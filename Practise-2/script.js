006a52882fa418491046f4d6a0d69f2c9bbd89d6
Sourav Sanjay <souravsanjay07@gmail.com>
2025-11-10 11:38:59 +0530

    First question done

---
+++ Practise-2/script.js
@@ -0,0 +1,18 @@
+
+// 1. Grocery Bill Calculator
+// Write a function calculateBill(items) that takes an array of item prices and returns the total bill including 5% GST.
+
+//----------------SOLUTION----------------//
+
+// function groceryBill(arr){
+//     let totalPrice = 0;
+//     let gst = 0.05;
+//     for(let i = 0; i <= arr.length - 1; i++){
+//         totalPrice += arr[i] + (arr[i] * gst);
+//     }
+//     console.log(totalPrice);
+// }
+
+// groceryBill([100,200,300,400,500]);
+
+//--------------QUESTION 2------------------//
